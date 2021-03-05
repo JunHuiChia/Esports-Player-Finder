d5e852644faf6eeab1798351d95a9551afe930e7
Josh Hamlet <72735315+UP929775@users.noreply.github.com>
2021-03-05 20:21:22 +0000

    Update activitys

--- MobileFrontend/app/src/main/java/com/example/esportsplayerfinder/MainActivity.java
+++ MobileFrontend/app/src/main/java/com/example/esportsplayerfinder/MainActivity.java
@@ -8,21 +8,34 @@ import androidx.annotation.ColorInt;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    ViewPager2 viewPager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.landing_page);
+        setContentView(R.layout.activity_landing);
+
+
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.Dashbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setLogo(R.drawable.logo6);
         getSupportActionBar().setTitle(R.string.Empty_String);
+
+
+
 
 
 
@@ -46,11 +59,11 @@ public class MainActivity extends AppCompatActivity {
         switch(item.getItemId()){
             case R.id.myProfile:
                 // NEED TO CHANGE TO PROFILE PAGE WHEN MADE
-                setContentView(R.layout.activity_main);
+                setContentView(R.layout.activity_landing);
                 return true;
             case R.id.friendsList:
                 // NEED TO CHANGE TO FRIENDS LIST WHEN MADE
-                setContentView(R.layout.activity_main);
+                setContentView(R.layout.activity_landing);
                 return true;
 
         }
