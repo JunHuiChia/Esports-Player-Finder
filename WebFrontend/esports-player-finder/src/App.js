5e7d89e4ffb427d3351eaf3bbd76f9eea0bcb7c3
UP932790 <up932790@github.com>
2021-03-07 00:26:53 +0000

    Add placeholder authentication to web frontend

--- WebFrontend/esports-player-finder/src/App.js
+++ WebFrontend/esports-player-finder/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-import './App.css';
+// import './App.css';
+import "./css/tailwind.css"
+
 
 import Header from './components/header/header.js'
+// import Header from './components/header/header.js'
+import { AppProvider } from "./contexts/AppContext"
+import AuthContainer from "./components/auth/AuthContainer"
 
 function App() {
   return (
-      <Header/>
+    <AppProvider>
+        <AuthContainer>
+            <Header/>
+        </AuthContainer>
+    </AppProvider>
   );
 }
 
