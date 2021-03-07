179725eee3e85c4f4cb64d298d0f408bbe2e67cf
UP932790 <up932790@github.com>
2021-03-07 00:24:53 +0000

    Add Laravel Sanctum for API authentication

--- Backend/esports-player-finder/routes/api.php
+++ Backend/esports-player-finder/routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/login', [App\Http\Controllers\UserController::class, 'login']);
+Route::post('/register', [App\Http\Controllers\UserController::class, 'register']);
+Route::get('/logout', [App\Http\Controllers\UserController::class, 'logout']);
