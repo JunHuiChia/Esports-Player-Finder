3de3242d0f3cd6abb266db892652c8d5fa05efc6
UP924230 <72032185+UP924230@users.noreply.github.com>
2021-03-08 18:21:44 +0000

    Adding files to get started with backend connection

---
+++ MobileFrontend/app/src/main/java/com/example/esportsplayerfinder/URLs.java
@@ -0,0 +1,10 @@
+package com.example.esportsplayerfinder;
+
+public class URLs {
+
+    private static final String ROOT_URL = "backend.setap.local/api/sanctum/token";
+
+    public static final String URL_REGISTER = ROOT_URL + "register";
+    public static final String URL_LOGIN= ROOT_URL + "login";
+
+}
