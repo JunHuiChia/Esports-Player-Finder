bd823f45eb834c820c1c9d613625e59204f94cea
Josh Hamlet <72735315+UP929775@users.noreply.github.com>
2021-03-08 17:34:52 +0000

    Update menu

--- MobileFrontend/app/src/main/java/com/example/esportsplayerfinder/MainActivity.java
+++ MobileFrontend/app/src/main/java/com/example/esportsplayerfinder/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_profile, menu);
+
         return true;
     }
 
