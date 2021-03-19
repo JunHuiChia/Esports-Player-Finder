eba31a97b8210dfba1fe9f1d12c305b57bdc78cf
Jeffery <up937100@myport.ac.uk>
2021-03-19 22:59:00 +0000

    Started on teams page

--- WebFrontend/esports-player-finder/src/components/teams/teams.js
+++ WebFrontend/esports-player-finder/src/components/teams/teams.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import './teams.css';
 
 export default class Teams extends React.Component{
 
     render(){
         return(
-            <div>
+            <div id="teamPage">
 
-                <div>Teams page</div>
+                <div id="teamList">Available Teams</div>
+                <div id="createTeam"> 
+                    <button className="createTeamBtn">Create team</button>
+                </div>
             </div>
         )
     }
