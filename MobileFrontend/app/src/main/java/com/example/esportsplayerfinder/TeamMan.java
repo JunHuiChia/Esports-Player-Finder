deb807468debcdcf1cd7bfa32b586306188abd40
Josh Hamlet <72735315+UP929775@users.noreply.github.com>
2021-03-30 20:50:34 +0100

    Update Team Details page

---
+++ MobileFrontend/app/src/main/java/com/example/esportsplayerfinder/TeamMan.java
@@ -0,0 +1,9 @@
+package com.example.esportsplayerfinder;
+
+public class TeamMan {
+
+    public static String teamName;
+    public static String discordChannelCode;
+    public static int gameID;
+
+}
